4d37534e5891185e841341f67371d62363d3a7fc
root <1255815114@qq.com>
2022-10-28 17:20:26 +0800

    负载均衡的规则配置

--- CloudTest1/src/main/java/com/example/cloudtest1/CloudTest1Application.java
+++ CloudTest1/src/main/java/com/example/cloudtest1/CloudTest1Application.java
@@ -1,5 +1,8 @@
 package com.example.cloudtest1;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -18,4 +21,10 @@ public class CloudTest1Application {
     RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    // 在该服务里面，调用其他任何服务都时这个规则，yml文件里的配置只针对调用某一个服务用的规则
+//    @Bean
+//    public IRule randomRule(){
+//        return new RandomRule();
+//    }
 }
