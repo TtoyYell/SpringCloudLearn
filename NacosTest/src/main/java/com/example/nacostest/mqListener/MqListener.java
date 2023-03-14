6d5d50256aeac105599bcd542cbc0b7007985e4e
Kanou <1255815114@qq.com>
2023-03-14 19:23:32 +0800

    mq学习

---
+++ NacosTest/src/main/java/com/example/nacostest/mqListener/MqListener.java
@@ -0,0 +1,13 @@
+package com.example.nacostest.mqListener;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Ye Tianyi
+ * @version 1.0
+ * @date 2023/3/9 21:45
+ */
+@Component
+public class MqListener {
+
+}
--- NacosTest/src/test/java/com/example/nacos/CloudTest2ApplicationTests.java
+++ NacosTest/src/test/java/com/example/nacos/CloudTest2ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.nacos;
 
+import com.example.nacostest.NacosApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = NacosApplication.class)
 class CloudTest2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
+    @Test
+    public void testSend(){
+        String queueName = "hello_world";
+        String message = "hello";
+        rabbitTemplate.convertAndSend(queueName,message);
+    }
 }
